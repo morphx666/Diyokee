c4b4715b0af4d5d9d70606f738db57cc38f37974
Xavier Flix <morphx666@users.noreply.github.com>
2025-04-08 15:01:00 -0400

    Add tag editor, bpm and downbeat adjustment with mini-player for playback preview

--- Classes/DFile.cs
+++ Classes/DFile.cs
@@ -4,12 +4,13 @@ namespace Diyokee {
     // Migrations:
     // dotnet ef migrations add "Added ReplayGain"
     // dotnet ef database update
-    public class DFile {
+    public class DFile : ICloneable {
         public int Id { get; set; }
         public string Artist { get; set; } = "";
         public string Title { get; set; } = "";
         public string Genre { get; set; } = "";
         public string Album { get; set; } = "";
+        public int Year { get; set; } = 0;
         public string Filename { get; set; } = "";
         public double Duration { get; set; } = 0;
         public string Waveform { get; set; } = ""; // Base64 encoded
@@ -29,15 +30,41 @@ namespace Diyokee {
             }
         }
 
-        public static string FormatTime(double seconds) {
+        public static string FormatTime(double seconds, bool includeMs = false) {
             double h = Math.Floor(seconds / 3600);
             double m = Math.Floor((seconds % 3600) / 60);
             double s = seconds % 60;
+            double ms = (s - Math.Floor(s)) * 1000;
             StringBuilder sb = new();
-            if(h > 0) sb.Append($"{h:00}:");
+            if(h > 0 || includeMs) sb.Append($"{h:00}:");
             sb.Append($"{m:00}:");
             sb.Append($"{s:00}");
+            if(includeMs) sb.Append($".{ms:000}");
             return sb.ToString();
+        }
+
+        public static double ParseTime(double h, double m, double s, double ms) {
+            return (h * 3600) + (m * 60) + s + (ms / 1000);
+        }
+
+        public object Clone() {
+            return this.MemberwiseClone();
+            return new DFile {
+                Id = Id,
+                Artist = Artist,
+                Title = Title,
+                Genre = Genre,
+                Album = Album,
+                Year = Year,
+                Filename = Filename,
+                Duration = Duration,
+                Waveform = Waveform,
+                BPM = BPM,
+                DownbeatAt = DownbeatAt,
+                Key = Key,
+                ReplayGain = ReplayGain,
+                HasReplayGain = HasReplayGain
+            };
         }
     }
 }
--- Migrations/20250408151455_Added Year.Designer.cs
+++ Migrations/20250408151455_Added Year.Designer.cs
@@ -2,6 +2,7 @@
 using Diyokee.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Diyokee.Migrations
 {
     [DbContext(typeof(CacheDbContext))]
-    partial class CacheDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250408151455_Added Year")]
+    partial class AddedYear
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.3");
