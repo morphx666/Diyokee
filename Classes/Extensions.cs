46807071d6dfa409f473883f29e0559c0b06bae3
Xavier Flix <morphx666@users.noreply.github.com>
2025-04-15 10:05:02 -0400

    Code refactoring

--- Classes/Extensions.cs
+++ Classes/Extensions.cs
@@ -1,10 +1,19 @@
 ﻿namespace Diyokee {
     public static class Extensions {
-        public static (T, int)[] ToArrayWithIndex<T>(this T[] list) {
+        public static (T, int)[] WithIndex<T>(this T[] array) {
             int i = 0;
-            (T, int)[] result = new (T, int)[list.Length];
-            foreach(T e in list) result[i] = (e, i++);
+            (T, int)[] result = new (T, int)[array.Length];
+            foreach(T e in array) result[i] = (e, i++);
             return result;
+        }
+
+        public static List<(T, int)> WithIndex<T>(this List<T> list) {
+            return [.. list.ToArray().WithIndex()];
+
+            //int i = 0;
+            //List<(T, int)> result = [];
+            //foreach(T e in list) result[i] = (e, i++);
+            //return result;
         }
     }
 }
