e8bb217dcf8f646f364be375d6627ca00be85a3b
Xavier Flix <morphx666@users.noreply.github.com>
2025-04-15 09:29:00 -0400

    Improve player settings handling

---
+++ Classes/Extensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Diyokee {
+    public static class Extensions {
+        public static (T, int)[] ToArrayWithIndex<T>(this T[] list) {
+            int i = 0;
+            (T, int)[] result = new (T, int)[list.Length];
+            foreach(T e in list) result[i] = (e, i++);
+            return result;
+        }
+    }
+}
