619902e649d42d90b41c96982c3fe1029e8cf35c
Xavier Flix <morphx666@users.noreply.github.com>
2025-05-14 14:09:06 -0400

    Simplify speakers matrix generation

--- Classes/AudioDevice.cs
+++ Classes/AudioDevice.cs
@@ -1,21 +1,21 @@
 ﻿namespace Diyokee {
     public class AudioDevice(string name, List<AudioDevice.DeviceSpeakers> speakers) {
         public enum DeviceSpeakers {
-            FrontStereo,
-            FrontLeft,
-            FrontRight,
+            FrontStereo     = 0b00000011,
+            FrontLeft       = 0b00000001,
+            FrontRight      = 0b00000010,
 
-            SideStereo,
-            SideLeft,
-            SideRight,
+            SideStereo      = 0b00001100,
+            SideLeft        = 0b00000100,
+            SideRight       = 0b00001000,
 
-            RearStereo,
-            RearLeft,
-            RearRight,
+            RearStereo      = 0b00110000,
+            RearLeft        = 0b00010000,
+            RearRight       = 0b00100000,
 
-            CenterAndLFE,
-            Center,
-            LFE
+            CenterAndLFE    = 0b11000000,
+            Center          = 0b01000000,
+            LFE             = 0b10000000,
         }
 
         public string Name { get; set; } = name;
