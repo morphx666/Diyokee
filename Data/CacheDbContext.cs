6a4a759590ef0984e51281d19d896844d3a194f3
Xavier Flix <morphx@gmail.com>
2026-02-24 23:47:41 -0400

    Refactor CacheDbContext constructor and connection string handling for non-Windows platforms

--- Data/CacheDbContext.cs
+++ Data/CacheDbContext.cs
@@ -1,15 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
 namespace Diyokee.Data {
-    public class CacheDbContext : DbContext {
-        protected readonly IConfiguration Configuration;
+    public class CacheDbContext(IConfiguration configuration) : DbContext {
+        protected readonly IConfiguration Configuration = configuration;
 
         public DbSet<DFile> Files { get; set; }
 
-        public CacheDbContext(IConfiguration configuration) => Configuration = configuration;
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
-            optionsBuilder.UseSqlite(Configuration.GetConnectionString("CacheDB"));
+            string connectionString = Configuration.GetConnectionString("CacheDB") ?? "Data Source=.\\Data\\cache.db";
+            string[] tokens = connectionString.Split('=');
+            tokens[1] = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, connectionString);
+            optionsBuilder.UseSqlite(string.Join("=", tokens));
         }
     }
 }
